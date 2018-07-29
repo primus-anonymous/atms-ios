d5909637647a2a63dd627299750990ed96f65b47
Vasilii Ponteleev <neocaptainnemo@gmail.com>
2018-07-29 21:47:10 +0200

    Added Travis CI

--- atmsTests/GeneratedMocks.swift
+++ atmsTests/GeneratedMocks.swift
@@ -1,4 +1,4 @@
-// MARK: - Mocks generated from file: atms/swift/services/AtmsProtocol.swift at 2018-07-29 19:06:39 +0000
+// MARK: - Mocks generated from file: atms/swift/services/AtmsProtocol.swift at 2018-07-29 19:45:12 +0000
 
 
 import Cuckoo
